cebeef260e6cd86646a8ce2d384e4e53425d7ceb
Rob Gleeson <robert.gleeson@softwaredesign.ie>
2018-05-24 12:31:37 +0100

    [impl] Lesson 30 - Set selected video state & display

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-  const { videos } = props;
+  const { videos, onVideoSelect } = props;
+  const VideoItems = videos.map((video) => {
+    return (
+      <VideoListItem
+        onVideoSelect={onVideoSelect}
+        key={video.etag} video={video} />
+    );
+  });
+
   return (
    <ul className='col-md-4 list-group'>
-    {videos.length}
+    {VideoItems}
    </ul>
   )
 }
